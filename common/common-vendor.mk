20ebb01a5729e08d0401e296c9720ad03b0edaba
AdarshGrewal <adarshgrewal@gmail.com>
2024-04-19 21:21:13 +0530

    miuicamera: Extract from garnet V816.0.6.0.UNRMIXM
    a

--- common/common-vendor.mk
+++ common/common-vendor.mk
@@ -5,3 +5,11 @@
 PRODUCT_SOONG_NAMESPACES += \
     vendor/xiaomi/garnet-miuicamera/common
 
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/garnet-miuicamera/common/proprietary/system/lib64/libcamera_algoup_jni.xiaomi.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libcamera_algoup_jni.xiaomi.so \
+    vendor/xiaomi/garnet-miuicamera/common/proprietary/system/lib64/libcamera_mianode_jni.xiaomi.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libcamera_mianode_jni.xiaomi.so \
+    vendor/xiaomi/garnet-miuicamera/common/proprietary/system/lib64/libmicampostproc_client.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libmicampostproc_client.so \
+    vendor/xiaomi/garnet-miuicamera/common/proprietary/system/lib64/vendor.xiaomi.hardware.campostproc@1.0.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/vendor.xiaomi.hardware.campostproc@1.0.so
+
+PRODUCT_PACKAGES += \
+    MiuiCamera
